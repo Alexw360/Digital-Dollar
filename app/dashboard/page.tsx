0b2e5d54963da0fa36da30d96191d24d197dec27
Alexw360 <alexw360@verizon.net>
2023-06-25 22:26:59 -0500

    Dashboard route created with a button element

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,13 @@
+import Button from '@/components/ui/Button'
+import { Ghost } from 'lucide-react'
+import { FC } from 'react'
+
+interface pageProps {
+  
+}
+
+const page: FC<pageProps> = ({}) => {
+  return <Button size='lg' variant='default'>Hello Button</Button>
+}
+
+export default page
